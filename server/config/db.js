f25e5741af1a6fc90fff8283fcd9f7f160a157c4
Roman Gagarin <gagarin.roma@gmail.com>
2021-10-28 15:20:20 -0400

    updated db URI to work with Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -7,5 +7,6 @@ Date: October 18th, 2021
 
 module.exports =
 {
-    "URI": "mongodb://localhost/portfolio_site"
+    //"URI": "mongodb://localhost/portfolio_site"
+    "URI": "mongodb+srv://Roman:C4jSt75sGv3QW2Qk@portfoliodb.kp5p0.mongodb.net/portfolio_site?retryWrites=true&w=majority"
 }
